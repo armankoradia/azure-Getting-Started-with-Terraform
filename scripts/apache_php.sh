9d1c7c24d75d5d3e3d39039e57fb71356ce6818e
Arman Koradia <ask.arman990@gmail.com>
2017-08-17 19:08:42 +0530

    Minor script adjustments

--- scripts/apache_php.sh
+++ scripts/apache_php.sh
@@ -15,8 +15,12 @@ echo "ServerName $PUB_IP" >> /etc/apache2/apache2.conf
 
 #Installing PHP and required modules
 echo "Installing php modules"
-apt-get -y install php libapache2-mod-php php-mcrypt php-mysql php56-mysqlnd
+echo "deb http://ftp.de.debian.org/debian stretch main" >> /etc/apt/sources.list
+apt-get -y install php7.0-fpm php7.0-cli php7.0-common libapache2-mod-php7.0 php7.0-mcrypt php7.0-mysql php5-mysqlnd
 
 #Setting permissions and ownership for /var/www
 echo "Setting ownership on /var/www"
 sudo chown -R www-data:www-data /var/www
+
+#Downloading demo.php file 
+wget https://github.com/armankoradia/azure-Getting-Started-with-Terraform/blob/master/scripts/demo.php -O /var/www/html/demo.php
