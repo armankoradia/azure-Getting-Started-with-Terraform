fd725b0979c2680e2185316865753041d3c18d78
Arman Koradia <ask.arman990@gmail.com>
2017-08-24 00:00:58 +0530

    Update pip_nic.tf

--- terraform_configs/pip_nic.tf
+++ terraform_configs/pip_nic.tf
@@ -13,12 +13,13 @@ resource "azurerm_network_interface" "public_nic" {
   name 					= "LA-Terraform-Web"
   location 				= "${var.location}"
   resource_group_name 	= "${azurerm_resource_group.terraform_rg.name}"
+  network_security_group_id = "${azurerm_network_security_group.nsg_web.id}"
   
   ip_configuration {
-    name 							= "LA-Terraform-WebPrivate"
-    subnet_id 					= "${azurerm_subnet.la_subnet_1.id}"
+    name 			  = "LA-Terraform-WebPrivate"
+    subnet_id 			  = "${azurerm_subnet.la_subnet_1.id}"
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id 			= "${azurerm_public_ip.la_pip.id}"
+    public_ip_address_id 	  = "${azurerm_public_ip.la_pip.id}"
   }
   tags {
 	group = "LinuxAcademy"
@@ -26,13 +27,14 @@ resource "azurerm_network_interface" "public_nic" {
 }
 
 resource "azurerm_network_interface" "private_nic" {
-  name 					= "LA-Terraform-DB"
-  location 				= "${var.location}"
+  name 			= "LA-Terraform-DB"
+  location 		= "${var.location}"
   resource_group_name 	= "${azurerm_resource_group.terraform_rg.name}"
+  network_security_group_id = "${azurerm_network_security_group.terraform_nsg_db.id}"
   
   ip_configuration {
-    name 							= "LA-Terraform-DBPrivate"
-    subnet_id 					= "${azurerm_subnet.la_subnet_2.id}"
+    name 			= "LA-Terraform-DBPrivate"
+    subnet_id 			= "${azurerm_subnet.la_subnet_2.id}"
     private_ip_address_allocation = "static"
   }
   tags {
