3af3657a2307fc45bbabd7d78f3dec541e48cb48
Arman Koradia <armank@sintecmedia.com>
2017-10-16 12:24:35 +0530

    minor update
    Minor update in pip_nic file regarding public_ip_address_id property.
    Works perfectly well with Terraform

--- terraform_configs/pip_nic.tf
+++ terraform_configs/pip_nic.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "la_pip" {
-  name 							= "LA-Terraform-PIP"
-  location 						= "${var.location}"
-  resource_group_name 			= "${azurerm_resource_group.terraform_rg.name}"
+  name 				= "LA-Terraform-PIP"
+  location 			= "${var.location}"
+  resource_group_name 		= "${azurerm_resource_group.terraform_rg.name}"
   public_ip_address_allocation 	= "static"
 
   tags {
@@ -10,19 +10,30 @@ resource "azurerm_public_ip" "la_pip" {
 }
 
 resource "azurerm_network_interface" "public_nic" {
-  name 					= "LA-Terraform-Web"
-  location 				= "${var.location}"
-  resource_group_name 	= "${azurerm_resource_group.terraform_rg.name}"
+  name 		      = "LA-Terraform-Web"
+  location 	      = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.terraform_rg.name}"
   network_security_group_id = "${azurerm_network_security_group.nsg_web.id}"
 
   ip_configuration {
-    name 			  = "LA-Terraform-WebPrivate"
-    subnet_id 			  = "${azurerm_subnet.la_subnet_1.id}"
+    name 			= "LA-Terraform-WebPrivate"
+    subnet_id 			= "${azurerm_subnet.la_subnet_1.id}"
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id 	  = "${azurerm_public_ip.la_pip.id}"
+    public_ip_address_id	= "${azurerm_public_ip.la_pip.id}"
   }
   tags {
 	group = "LinuxAcademy"
+  }
+}
+
+resource "azurerm_public_ip" "la_db_pip" {
+  name                  = "LA-Terraform-DB-PIP"
+  location              = "${var.location}"
+  resource_group_name   = "${azurerm_resource_group.terraform_rg.name}"
+  public_ip_address_allocation = "dynamic"
+
+  tags {
+        group = "LinuxAcademy"
   }
 }
 
@@ -37,6 +48,7 @@ resource "azurerm_network_interface" "private_nic" {
     subnet_id 			= "${azurerm_subnet.la_subnet_2.id}"
     private_ip_address_allocation = "static"
     private_ip_address = "192.168.2.5"
+    public_ip_address_id = "${azurerm_public_ip.la_db_pip.id}"
   }
   tags {
 	group = "LinuxAcademy"
